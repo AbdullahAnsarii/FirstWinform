802a1beecc91b5785ec5a35fe91a7c00dc715edd
Abdullah Ansari <aa9272137@gmail.com>
2020-07-31 02:57:27 +0500

    Completed my first wform

---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'AppData.Intro' table. You can move, or remove it, as needed.
+        Me.IntroTableAdapter.Fill(Me.AppData.Intro)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Registration Succesfull....", "Abdullah Company")
+    End Sub
+End Class
